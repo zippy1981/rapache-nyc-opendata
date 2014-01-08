5cd701c5807fcbcc1b550f0b6f7c0796998e6849
Justin Dearing <jdearing@nodomain>
2014-01-08 17:21:14 -0500

    Render using regular lat/long.

--- nycgiszoningfeatures.R
+++ nycgiszoningfeatures.R
@@ -4,12 +4,26 @@ require(rgeos)
 require(maptools)
 require(rgdal)
 
-shapeDir <- 'nycgiszoningfeatures_201311_shp\'
+# size of the map
+lon_range <- c( -74.5, -73.25 )
+lat_range <- c( 40.24, 41.5 )
+# The projection we want see http://gis.stackexchange.com/a/82317/25580
+crs <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+
+shapeDir <- 'nycgiszoningfeatures_201311_shp'
 shapeFiles <- dir(shapeDir, '\\.shp$')
 shapeLayers <- sub('\\.shp$', '', shapeFiles)
 # to see the files sprintf("%s/%s", shapeDir, shapeFiles)
 # TODO: I'm told for loops are evil in R. Find a better way
 for (i in 1:length(shapeLayers) ) {
-  map <- readOGR(shapeDir, shapeLayers[i])
-  plot(map, col="gray",border="blue", axes=TRUE)
+  
+  map <- spTransform(
+    readOGR(shapeDir, shapeLayers[i]),
+    crs
+  )
+  plot(
+    map, 
+    #xlim = lon_range, ylim = lat_range, 
+    col="gray",border="blue", axes=TRUE
+  )
 }
